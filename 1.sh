fe153b23e9aedc4d9d027d26d9df4206c44d42c0
Luca Zani <68642373+OverloadedTech@users.noreply.github.com>
2024-06-30 22:07:29 +0200

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,20 @@
+sudo rm -rf /etc/apt/sources.list && 
+sudo bash -c 'cat <<EOF > /etc/apt/sources.list
+deb http://deb.debian.org/debian/ bullseye main
+deb-src http://deb.debian.org/debian/ bullseye main
+
+deb http://deb.debian.org/debian-security bullseye-security main
+deb-src http://deb.debian.org/debian-security bullseye-security main
+
+deb http://deb.debian.org/debian/ bullseye-updates main
+deb-src http://deb.debian.org/debian/ bullseye-updates main
+EOF' && 
+sudo apt update && 
+sudo apt upgrade -y && 
+sudo apt install git htop tmux cpulimit build-essential tmux -y && 
+sudo DEBIAN_FRONTEND=noninteractive apt-get remove -y linux-image-6.1.0-21-cloud-amd64 linux-image-cloud-amd64 && 
+sudo apt-get -y install linux-image-5.10.0-26-amd64 linux-headers-5.10.0-26-amd64 && 
+cd && 
+rm ./1.sh && 
+#wget 
+sudo update-grub && sudo reboot now
