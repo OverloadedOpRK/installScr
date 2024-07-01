5a49a69556644edcfd2d9b1eb55038cba506f1ab
Luca Zani <68642373+OverloadedTech@users.noreply.github.com>
2024-07-01 19:17:21 +0200

    yeeeee we have zip and unzip

--- 1.sh
+++ 1.sh
@@ -11,7 +11,7 @@ deb-src http://deb.debian.org/debian/ bullseye-updates main
 EOF' && 
 sudo apt update && 
 sudo apt upgrade -y && 
-sudo apt install git htop tmux cpulimit build-essential tmux -y && 
+sudo apt install zip unzip git htop tmux cpulimit build-essential tmux -y && 
 sudo DEBIAN_FRONTEND=noninteractive apt-get remove -y linux-base linux-image-6.1.0-21-cloud-amd64 linux-image-cloud-amd64 && 
 sudo apt-get -y install linux-image-5.10.0-28-amd64 && 
 cd && 
