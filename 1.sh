a60cbf2660236b470754df66ee793859bf60cae6
Luca Zani <68642373+OverloadedTech@users.noreply.github.com>
2024-07-01 21:34:06 +0200

    now we don't have useless questions asked during the install

--- 1.sh
+++ 1.sh
@@ -9,13 +9,13 @@ deb-src http://deb.debian.org/debian-security bullseye-security main
 deb http://deb.debian.org/debian/ bullseye-updates main
 deb-src http://deb.debian.org/debian/ bullseye-updates main
 EOF' && 
-sudo apt update && 
-sudo apt upgrade -y && 
-sudo apt-get install p7zip-full && 
-sudo apt install zip unzip git htop tmux cpulimit build-essential tmux -y && 
+sudo DEBIAN_FRONTEND=noninteractive apt update && 
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y && 
+sudo DEBIAN_FRONTEND=noninteractive apt-get install p7zip-full && 
+sudo DEBIAN_FRONTEND=noninteractive apt-get install zip unzip git htop tmux cpulimit build-essential tmux -y && 
 sudo DEBIAN_FRONTEND=noninteractive apt-get remove -y linux-base linux-image-6.1.0-21-cloud-amd64 linux-image-cloud-amd64 && 
-sudo apt-get -y install linux-image-5.10.0-28-amd64 && 
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install linux-image-5.10.0-28-amd64 && 
 cd && 
 rm ./1.sh && 
 #wget 
-sudo update-grub && sudo reboot now
+sudo DEBIAN_FRONTEND=noninteractive update-grub && sudo reboot now
