7ece8f8a1214736113127b0426b167f8fb7b5b4f
Luca Zani <68642373+OverloadedTech@users.noreply.github.com>
2024-07-01 22:23:51 +0200

    test "please work"

--- 1.sh
+++ 1.sh
@@ -1,11 +1,11 @@
-#NOW IT WORKS
-
-
-#sudo rm -rf /etc/apt/sources.list && 
+sudo rm -rf /etc/apt/sources.list && 
 sudo bash -c 'cat <<EOF > /etc/apt/sources.list
-
-deb http://deb.debian.org/debian bookworm-backports main contrib non-free
-
+deb http://deb.debian.org/debian/ bullseye main
+deb-src http://deb.debian.org/debian/ bullseye main
+deb http://deb.debian.org/debian-security bullseye-security main
+deb-src http://deb.debian.org/debian-security bullseye-security main
+deb http://deb.debian.org/debian/ bullseye-updates main
+deb-src http://deb.debian.org/debian/ bullseye-updates main
 EOF' && 
 sudo DEBIAN_FRONTEND=noninteractive apt update && 
 sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y && 
@@ -14,6 +14,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install zip unzip git htop tmux cpul
 sudo DEBIAN_FRONTEND=noninteractive apt-get remove -y linux-base linux-image-6.1.0-21-cloud-amd64 linux-image-cloud-amd64 && 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install linux-image-5.10.0-28-amd64 && 
 cd && 
+#wget 2.sh && 
 rm ./1.sh && 
-#wget 
 sudo DEBIAN_FRONTEND=noninteractive update-grub && sudo reboot now
